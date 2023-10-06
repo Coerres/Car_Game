7431acc54fd6995a49b8cdbb42e257e2d8bd34e6
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-10-06 09:43:46 +0200

    added border and cursor functions

--- cargame.cpp
+++ cargame.cpp
@@ -24,3 +24,27 @@ char car[4][5] = { ' ','±','±',' ',
 int carPos = WIN_WIDTH/2;
 int score = 0;
 
+void gotoxy(int x, int y){
+    CursorPosition.X = x;
+    CursorPosition.Y = y;
+    SetConsoleCursorPosition(console, CursorPosition);
+}
+void setcursor(bool visible, DWORD size){
+    if(size == 0)
+        size = 20;
+
+    CONSOLE_CURSOR_INFO lpCursor;
+    lpCursor.bVisible = visible;
+    lpCursor.dwSize = size;
+    SetConsoleCursorInfo(console,&lpCursor);
+}
+void drawBorder(){
+    for(int i=0; i<SCREEN_HEIGHT; i++){
+        for(int j=0; j<17; j++){
+            gotoxy(0+j,i); cout<<"±";
+        }
+    }
+    for(int i=0; i<SCREEN_HEIGHT; i++){
+        gotoxy(SCREEN_WIDTH,i); cout<<"±";
+    }
+}
