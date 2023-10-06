7db72ea3d1dbca1893f31ba2219191d4edec03e2
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-10-06 09:10:31 +0200

    setup

---
+++ cargame.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+#include<dos.h>
+#include<windows.h>
+#include<time.h>
+
+#define SCREEN_WIDTH 90
+#define SCREEN_HEIGHT 26
+#define WIN_WIDTH 70
+
+using namespace std;
